2da99f345f3f969b23a4de948ce67a94046cf9c8
aparnaashoks <96467199+aparnaashoks@users.noreply.github.com>
2022-03-21 15:14:44 +0530

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,22 @@
+import java .util.*;
+class Program
+{
+public static void main(String  [] args)
+{
+int age;
+char Name[25];
+float salary;
+Scanner s1=new Scanner(System.in);
+System.out.println("Enter the name");
+a=s1.nextLine();
+System.out.println("Enter the age ");
+a=s1.nextInt();
+System.out.println("Enter the salary");
+a=s1.nextFloat();
+System.out.println(" Name"+Name);
+System.out.println("Age"+Age);
+System.out.println("Salary"+Salary);
+}
+}
+
+
