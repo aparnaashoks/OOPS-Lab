d189be4d1b6affef034f0836424e656fd8dbf8fd
Aparna Ashok <96467199+aparnaashoks@users.noreply.github.com>
2022-05-18 11:10:53 +0530

    Add files via upload

---
+++ Extra programs/Add.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Add
+{
+public static void main(String []args)
+{
+int a,b,c;
+Scanner s1=new Scanner(System.in);
+System.out.println("Enter two Numbers");
+a=s1.nextInt();
+b=s1.nextInt();
+c=a+b;
+System.out.println("Result is"+c);
+}
+}
